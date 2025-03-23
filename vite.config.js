4cc4a2f61311b6acaae4fbbd16bd979d852cb8e3
numan-mirjankar <numanmirjankar@gmail.com>
2025-03-23 23:32:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/book-list/", // 🔥 Add this line (must match your repo name)
+});
+
