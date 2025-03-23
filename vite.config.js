7d37f20cb109312ccc9de3a4ab08856f5cc5154a
numan-mirjankar <numanmirjankar@gmail.com>
2025-03-23 23:41:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BookList/", // 🔥 Add this line (must match your repo name)
+  base: "/book-list/", // 👈 Add this (MUST match the repository name)
 });
-
